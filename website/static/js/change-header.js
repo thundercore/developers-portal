6b15346af4914f921605794238a34d7bd7f8e0d6
Bean <bean@thundertoken.tw>
2019-12-23 18:16:23 +0800

    change docs folder sctructure

---
+++ website/static/js/change-header.js
@@ -0,0 +1,11 @@
+var path = location.pathname.substring(1);
+
+if (!!path.includes("docs/suppoer-center")){
+  var timeoutID = window.setInterval(( () => {
+    var img = document.getElementsByClassName("logo")[0]
+    img.setAttribute('src',"/img/thundercore_icon_white.svg")
+    if(img.src.includes("/img/thundercore_icon_white.svg") ) {
+      window.clearInterval(timeoutID);
+    }
+  }), 100);
+};
