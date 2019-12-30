9e591174a8c33eb8984300b2e90ee36f40a55aa7
Bean <bean@thundertoken.tw>
2019-12-30 20:05:21 +0800

    fix wrong redirect

--- redirect.sh
+++ redirect.sh
@@ -22,8 +22,8 @@ do
   touch $filename.html
   mkdir $filename
   
-  aws s3 cp $filename.html s3://$url/docs/$path --website-redirect "$redirect_url/docs/$redirect_path"
-  aws s3 cp --recursive $filename s3://$url/docs/$path --website-redirect "$redirect_url/docs/$redirect_path"
+  aws s3 cp $filename.html s3://$url/docs/$path --website-redirect "${PROTOCAL}://$redirect_url/docs/$redirect_path"
+  aws s3 cp --recursive $filename s3://$url/docs/$path --website-redirect "${PROTOCAL}://$redirect_url/docs/$redirect_path"
 done
 
 # from sc to dev
@@ -33,11 +33,11 @@ filename="deploy-your-own-game"
 touch $filename.html
 mkdir $filename
 
-aws s3 cp $filename.html s3://$redirect_url/docs/$redirect_path --website-redirect "$url/docs/$path"
-aws s3 cp --recursive $filename s3://$redirect_url/docs/$redirect_path --website-redirect "$url/docs/$path"
+aws s3 cp $filename.html s3://$redirect_url/docs/$redirect_path --website-redirect "${PROTOCAL}://$url/docs/$path"
+aws s3 cp --recursive $filename s3://$redirect_url/docs/$redirect_path --website-redirect "${PROTOCAL}://$url/docs/$path"
 
 # direct to sc
 redirect_path="faqs/general"
 touch index.html
 
-aws s3 cp index.html s3://$redirect_url/ --website-redirect "$redirect_url/docs/$redirect_path"
+aws s3 cp index.html s3://$redirect_url/ --website-redirect "${PROTOCAL}://$redirect_url/docs/$redirect_path"
