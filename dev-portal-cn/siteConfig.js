7639bf12e45d2609f95fb2e40ea5b737ddfeb9e3
Johnny <48502835+pdafight@users.noreply.github.com>
2020-02-05 17:02:59 +0800

    update header url

--- dev-portal-cn/siteConfig.js
+++ dev-portal-cn/siteConfig.js
@@ -26,7 +26,7 @@ const siteConfig = {
   favicon: 'img/favicon/favicon.ico',
 
   headerLinks: [
-    { href: 'https://thundercore.com', label: 'ThunderCore.com' },
+    { href: 'https://www.thundercore.com', label: 'ThunderCore.com' },
     { doc: 'deploy-your-own-game', label: '开发者中心' },
     { doc: 'faqs/general', label: '用户支持中心' },
     { href: 'https://developer.thundercore.com', label: '🌐English'}
