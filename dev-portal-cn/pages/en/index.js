89fb0fdddbad6db78faba5352143c7e20f6fccc4
Manesh <manesh@thundercore.tw>
2020-12-04 19:09:20 +0800

    update dev portal footer link

--- dev-portal-cn/pages/en/index.js
+++ dev-portal-cn/pages/en/index.js
@@ -109,7 +109,9 @@ class JumpStart extends React.Component {
 
         <form className="jumpStartMain"
           action="https://docs.google.com/forms/d/e/1FAIpQLSeHfya9a8MqO3h015hvH6JG9t57kowIFwZFW0IdZ6XufvEOLA/viewform"
-          method="post">
+          method="post" id="mc-embedded-subscribe-form"
+          name="mc-embedded-subscribe-form" className="validate" target="_blank"
+          noValidate>
 
           <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true">
             <input type="text" name="b_9e820737f58c3f0aa1940427b_3338803d92" tabIndex="-1" value="" />
@@ -122,7 +124,7 @@ class JumpStart extends React.Component {
           </div>
 
           <div className="jumpStartEmail">
-            <input type="submit" className="subscribeButton orangeButton" value="Submit Now!"/>
+            <input type="submit" className="subscribeButton orangeButton" value="<a href="https://docs.google.com/forms/d/e/1FAIpQLSeHfya9a8MqO3h015hvH6JG9t57kowIFwZFW0IdZ6XufvEOLA/viewform">Submit Now!</a>"/>
           </div>
         </form>
       </div>
