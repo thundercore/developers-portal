db2566e9638a8ed89d5322915f8dccdb55837cca
David Hung <david.hung@thundertoken.tw>
2020-12-09 16:13:44 +0800

    dev-portal en testing

--- dev-portal/pages/en/index.js
+++ dev-portal/pages/en/index.js
@@ -118,8 +118,9 @@ class JumpStart extends React.Component {
           <div className="jumpStartContent">
               The goal of the ThunderCore Developer Jumpstart Program is to get you through the first stage of developing your crypto DApp or building your startup.
           </div>
-          <div className="jumpStartEmail">
-          </div>
+           <div className="jumpStartEmail">
+                     <input type="submit" className="subscribeButton orangeButton" value="Submit Now!"/>
+                   </div>
         </form>
       </div>
     )
